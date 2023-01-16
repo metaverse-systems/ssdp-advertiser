739b3b3dd4f0e0979bc9fa22d868ed48f354f2c6
Tim Schwartz <tim@metaverse.systems>
2023-01-15 20:30:12 -0600

    moved web function into separate file

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,9 @@
 #include <pugixml.hpp>
-#include <curl/curl.h>
+#include "fetch.hpp"
 #include <libgssdp/gssdp.h>
 #include <gio/gio.h>
 #include <iostream>
-#include <fstream>
 #include <vector>
-
-static size_t callback(void *ptr, size_t size, size_t nmemb, void *str)
-{
-    size_t real_size = size * nmemb;
-
-    std::string in;
-    in.resize(real_size);
-    memcpy(&in[0], ptr, real_size);
-
-    std::string *output = (std::string *)str;
-    *output += in;
-
-    return real_size;
-}
-
-std::string fetch(std::string url)
-{
-    std::string output;
-
-    CURL *curl;
-    CURLcode res;
-    curl = curl_easy_init();
-    if(!curl) throw "Unable to initialize CURL";
-
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &output);
-    res = curl_easy_perform(curl);
-    if(res != CURLE_OK)
-    {
-        std::string error = "curl_easy_perform() failed: " + std::string(curl_easy_strerror(res));
-        throw error;
-    }
-
-    curl_easy_cleanup(curl);
-
-    return output;
-}
 
 std::vector<std::string> explode(char delim, std::string str)
 {
